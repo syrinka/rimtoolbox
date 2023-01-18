ccef4b67f9b2f67483f8739d1752ad0375c011c9
syrinka <syrinka@foxmail.com>
2023-01-18 17:07:13 +0800

    flush log every time

--- rimtoolbox/log.py
+++ rimtoolbox/log.py
@@ -4,7 +4,12 @@ from loguru import logger
 
 logger.remove()
 logger.add(sys.stderr, level=10)
-logger.add('run.log', level=5)
+# flush log every time
+# shall be configurable later
+logger.add(
+    open('run.log', 'w', encoding='utf-8'),
+    level=5
+)
 
 # install
 __builtins__['logger'] = logger
