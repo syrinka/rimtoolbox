3776789889b6fc0a92e62a56b4ba938c25c304bf
syrinka <syrinka@foxmail.com>
2023-01-18 19:37:27 +0800

    refactor extract.py

--- extract.py
+++ extract.py
@@ -3,20 +3,9 @@ from rimtoolbox.parser import Workspace, Path
 
 if __name__ == '__main__':
     w = Workspace()
-
-    defpath = sys.argv[1]
-    w.digest_dir(defpath / 'ThingDefs_Items')
-    w.digest_dir(defpath / 'ThingDefs_Buildings')
-    w.digest_dir(defpath / 'ThingDefs_Plants')
-    w.digest_dir(defpath / 'ThingDefs_Races')
-    w.digest_dir(defpath / 'ThingDefs_Misc', filter=lambda p: 'Ethereal' not in p.name)
-    w.digest_dir(defpath / 'Drugs')
-    w.digest_dir(defpath / 'HediffDefs')
-
-    langpath = sys.argv[2]
-    w.digest_dir(langpath / 'DefInjected' / 'ThingDef')
-
+    for i in sys.argv[1:-1]:
+        w.digest_dir(i)
     w.solve_inherit()
     w.inject_langdata(ignore_extra=True)
     w.clean()
-    w.dump('dump.json', indent=4)
+    w.dump(sys.argv[-1], indent=4)
